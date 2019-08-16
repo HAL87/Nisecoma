39d17ce97239bb67bc516b29d1a4c30b750ff1f1
HAL87 <hal0409galaxy@gmail.com>
2019-08-16 20:22:19 +0900

    tesutodesu

---
+++ Assets/Assets/Scripts/RouletteMaker/RouletteMaker.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RouletteMaker : MonoBehaviour
+{
+    //ルーレットの表示場所
+    [SerializeField] private Transform imageParentTransform;
+    //ルーレットのワザ名
+    [SerializeField] private List<string> moveNames;
+    //ルーレットの打点、星
+    [SerializeField] private List<string> movePowers;
+    //各ルーレットの色
+    [SerializeField] private List<Color> rouletteColors;
+    [SerializeField] private List<float> rouletteRange;
+    //白い円の画像
+    [SerializeField] private Image rouletteImage;
+
+    private float totalRange = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < moveNames.Count; i++)
+        {
+            Debug.Log(totalRange);
+            //指定位置に白い円の画像を生成
+            var obj = Instantiate(rouletteImage, imageParentTransform);
+            //ルーレットの色を決定
+            //obj.color = rouletteColors[(moveNames.Count - 1 - i)];
+            obj.color = rouletteColors[i];
+            //ワザの名前を入力
+            Text[] texts = obj.GetComponentsInChildren<Text>();
+            texts[0].text = moveNames[i];
+            texts[1].text = movePowers[i];
+            //obj.GetComponentInChildren<Text>().text = moveNames[i];
+            //obj.GetComponentInChildren<Text>().text = movePowers[i];
+ 
+            obj.fillAmount = 1 - totalRange /96;
+ 
+            obj.transform.GetChild(0).transform.rotation = Quaternion.Euler(0, 0, (rouletteRange[i] / 2 + totalRange) * 15 / 4);
+            totalRange += rouletteRange[i];
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Assets/Scripts/Spin.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spin : MonoBehaviour
+{
+    //回転している時間(ms)
+    private int rotateTime = 1000;
+    //回転数
+    private int rotateCount = 15;
+    //速度遅くなってから回る角度
+    float addAngle = 180;
+    float addTime = 500;
+
+    //累計回転角度
+    private float elapsedAngle;
+    //累計経過時間
+    private float elapsedTime;
+
+    //FixedUpdate毎(0.01秒)の回転角
+    private float rotateAngle;
+    //合計回転角度
+    float totalAngle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        elapsedAngle = 0;
+        elapsedTime = 0;
+        rotateAngle = 0;
+        totalAngle = 0;
+        //初期位置から目標点までの角度
+        int goalAngle = Random.Range(0, 361);
+
+        totalAngle = 360 * rotateCount + goalAngle;
+
+        rotateAngle = totalAngle / (rotateTime / 10);
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Debug.Log(elapsedTime);
+        elapsedAngle += rotateAngle;
+        if (elapsedAngle < totalAngle)
+        {
+            transform.Rotate(new Vector3(0, rotateAngle, 0));
+        }
+        else if (elapsedTime < addTime)
+        {
+            elapsedTime += Time.deltaTime * 1000;
+            //Debug.Log(elapsedTime);
+            transform.Rotate(new Vector3(0, rotateAngle - rotateAngle * elapsedTime / addTime, 0));
+        }
+        if (elapsedTime >= addTime)
+        {
+            elapsedAngle = 0;
+            elapsedTime = 0;
+            rotateAngle = 0;
+            totalAngle = 0;
+            int goalAngle = Random.Range(0, 361);
+
+            totalAngle = 360 * rotateCount + goalAngle;
+
+            rotateAngle = totalAngle / (rotateTime / 10);
+            GetComponent<Spin>().enabled = false;
+        }
+    }
+}

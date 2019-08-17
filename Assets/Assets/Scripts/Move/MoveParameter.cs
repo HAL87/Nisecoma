8a50fe7c5c38cfe42815ae6af3cfe914f2a32040
HAL87 <hal0409galaxy@gmail.com>
2019-08-17 16:28:46 +0900

    disk描画とデータベースを分けた

---
+++ Assets/Assets/Scripts/Move/MoveParameter.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveParameter : MonoBehaviour
+{
+    public enum MoveOfColorName
+    {
+        White,
+        Purple,
+        Blue,
+        Gold,
+        Red
+    };
+    [SerializeField] private string moveName;
+    [SerializeField] private int moveRange;
+    [SerializeField] private MoveOfColorName moveOfColorName;
+    [SerializeField] private Color color;
+    [SerializeField] private int movePower;
+    [SerializeField] private int moveNumberOfStar;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public string GetMoveName()
+    {
+        return moveName;
+    }
+
+    public void SetMoveRange(int _moveRange)
+    {
+        moveRange = _moveRange;
+    }
+    public int GetMoveRange()
+    {
+        return moveRange;
+    }
+
+    public MoveOfColorName GetMoveColorName()
+    {
+        return moveOfColorName;
+    }
+    public Color GetColor()
+    {
+        return color;
+    }
+
+
+    public void SetMovePower(int _movePower)
+    {
+        movePower =  _movePower;
+    }
+    public int GetMovePower()
+    {
+        return movePower;
+    }
+
+    public void SetMoveNumberOfStar(int _moveNumberOfStar)
+    {
+        moveNumberOfStar = _moveNumberOfStar;
+    }
+    public int GetMoveNumberOfStar()
+    {
+        return moveNumberOfStar;
+    }
+    
+}
--- Assets/Assets/Scripts/MoveName/HyperSonic.cs
+++ Assets/Assets/Scripts/MoveName/HyperSonic.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BlueMove : MonoBehaviour
+public class HyperSonic : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()

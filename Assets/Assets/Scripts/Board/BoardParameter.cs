b75049b5a4b32698b20fff836d33eb5fb448ec76
HAL87 <hal0409galaxy@gmail.com>
2019-08-31 14:54:40 +0900

    ボード作りかけ

---
+++ Assets/Assets/Scripts/Board/BoardParameter.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardParameter : MonoBehaviour
+{
+    //左上が0番で右上が6番、右下が27番や
+    [SerializeField] private int boardID;
+    //エントリーとかゴールとかそういう情報
+    //エッジ情報もいるのかなあ
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public int GetBoardID()
+    {
+        return boardID;
+    }
+}

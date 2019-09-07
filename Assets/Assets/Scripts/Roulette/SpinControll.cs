3532d149cd051b4146fe8165abb7be942a94d8f6
yongnam_k <yongnampoke@gmail.com>
2019-09-07 17:00:05 +0900

    SpinSceneで回したあと技名出るように

--- Assets/Assets/Scripts/Roulette/SpinControll.cs
+++ Assets/Assets/Scripts/Roulette/SpinControll.cs
@@ -7,7 +7,8 @@ public class SpinControll : MonoBehaviour
 {
     private RouletteMaker ruletteMaker0;
     private RouletteMaker ruletteMaker1;
-    private DiskSpin diskSpin;
+    private DiskSpin diskSpin0;
+    private DiskSpin diskSpin1;
     private GetInfo getInfo;
     [SerializeField] private List<GameObject> datadisks;
     // Start is called before the first frame update
@@ -21,13 +22,14 @@ public class SpinControll : MonoBehaviour
         //Debug.Log(getInfo.GetData(0));
         //ルーレット作成メソッドの呼び出し
         ruletteMaker0.CreateRulette(getInfo.GetData(0));
-        diskSpin = datadisks[0].GetComponent<DiskSpin>();
+        diskSpin0 = datadisks[0].GetComponent<DiskSpin>();
+        diskSpin1 = datadisks[1].GetComponent<DiskSpin>();
     }
 
     public void SpinStart()
     {
         
-        StartCoroutine(diskSpin.Spin(getInfo.GetData(0)));
+        StartCoroutine(diskSpin0.Spin(getInfo.GetData(0)));
     }
 
     // Update is called once per frame

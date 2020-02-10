de26308365f8beb1ea81e7e08af83bc24f3d4b08
Toshiki Hatanaka <hal0409galaxy@gmail.com>
2020-02-11 02:58:55 +0900

    アナログ統合準備 フォントは抜いてる

---
+++ Assets/Assets/Scripts/Analog/Attack.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack
+{
+
+    public string name;
+    public string color;
+    public int damage;
+    public int numberOfStar;
+    public int size;
+
+    public Attack(string name, string color, int damage, int numberOfStar, int size)
+    {
+        this.name = name;
+        this.color = color;
+        this.damage = damage;
+        this.numberOfStar = numberOfStar;
+        this.size = size;
+    }
+}
